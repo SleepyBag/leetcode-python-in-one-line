50e2eb9be09d16606cbf1c91ca902d9d58b1f267
Xue Qianming <39435859+SleepyBag@users.noreply.github.com>
2020-12-09 21:56:49 +0800

    Create 141.linked-list-cycle.py

---
+++ 141.linked-list-cycle.py
@@ -0,0 +1,3 @@
+class Solution:
+    def hasCycle(self, head: ListNode) -> bool:
+        return False if head is None else True if (head.val == 'foobar', head.__setattr__('val', 'foobar'))[0] else self.hasCycle(head.next)

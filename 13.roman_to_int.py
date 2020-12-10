565672d33f6dd6acfb8a0fc9f3374906250b8437
Xue Qianming <39435859+SleepyBag@users.noreply.github.com>
2020-12-10 13:39:16 +0800

    Update 13.roman_to_int.py

--- 13.roman_to_int.py
+++ 13.roman_to_int.py
@@ -1,3 +1,3 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        return sum([{'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}[c] for c in s]) + sum([{'IV': -2, 'IX': -2, 'XL': -20, 'XC': -20, 'CD': -200, 'CM': -200}[c1 + c2] for c1, c2 in zip(s, s[1:]) if c1 + c2 in ['IV', 'IX', 'XL', 'XC', 'CD', 'CM']])
+        return sum({'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}[c] for c in s) + sum(collections.defaultdict(lambda: 0, {'IV': -2, 'IX': -2, 'XL': -20, 'XC': -20, 'CD': -200, 'CM': -200})[c1 + c2] for c1, c2 in zip(s, s[1:]))
